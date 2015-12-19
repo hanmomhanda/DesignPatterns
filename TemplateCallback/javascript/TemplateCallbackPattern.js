79726587d147d9a8cc7a4b706393b1d2d632ac1d
hanmomhanda <hanmomhanda@gmail.com>
2015-12-19 17:10:44 +0900

    TemplateCallback 패턴 자바스크립트 완료

--- TemplateCallback/javascript/TemplateCallbackPattern.js
+++ TemplateCallback/javascript/TemplateCallbackPattern.js
@@ -2,20 +2,55 @@
  * 일반적인 웹 환경에서의 JavaScript는 파일을 직접 다룰 수 없으므로
  * Java에서의 File 관련 Exception을 단순히 모사해서 작성
  */
-var Template = {
-    processFile: function(callback){
-        readLine(file);
-        callback();
-        releaseFile();
+var readLine = function(filePath){
+    console.log(filePath + " is open and being read");
+    return [1, 2, 3, 4];
+};
+
+var releaseFile = function(filePath){
+    console.log(filePath + " is closed and released");
+};
+
+var template = {
+    processFile: function(filePath, initVal, callback){
+        var lines = readLine(filePath);
+        var result = initVal;
+        var i, len = lines.length;
+        for (i = 0 ; i < len ; i++){
+            result = callback(lines[i], result);
+        }
+        console.log(result);
+        releaseFile(filePath);
     }
 };
+
+
+
+(function(){
+    var filePath = "TemplateCallback/java/data.txt";
+    var lines =
+    template.processFile(filePath, 0, function(currVal, prevVal){
+        return currVal + prevVal;
+    });
+    template.processFile(filePath, 1, function(currVal, prevVal){
+        return currVal * prevVal;
+    });
+    template.processFile(filePath, "", function(currVal, prevVal){
+        return currVal + prevVal;
+    });
+})();
 
 // JavaScript는 함수를 전달할 수 있으므로
 // 굳이 Callback객체를 만들 필요는 없으나
 // Callback을 사용하는 입장에서 형 검사를 할 수 있게 하기위해
 // 인터페이스 역할을 하는 Callback 객체 생성
-var Callback = function(){};
-Callback.prototype.processFileContents = function(){
-    throw new Error("This method should be overridden");
-};
+//var Callback = function(){};
+//Callback.prototype.processFileContents = function(){
+//    throw new Error("This method should be overridden");
+//};
+
+// JavaScript는 함수를 전달할 수 있으므로
+// 굳이 Callback객체를 만들 필요는 없이
+// Callback 함수만 넘기면 된다
+
 
