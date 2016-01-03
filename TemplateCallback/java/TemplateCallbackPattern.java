666a1ce99637010baf16150022204bd1b5bc78a8
hanmomhanda <hanmomhanda@gmail.com>
2016-01-04 01:09:57 +0900

    파이썬 템플릿콜백패턴 시작

--- TemplateCallback/java/TemplateCallbackPattern.java
+++ TemplateCallback/java/TemplateCallbackPattern.java
@@ -5,7 +5,7 @@ import java.io.*;
 public class TemplateCallbackPattern {
 
     public static void main(String[] args){
-        String filePath ="TemplateCallback/java/data.txt";
+        String filePath = "TemplateCallback/data.txt";
 
         Template template = new Template();
 
