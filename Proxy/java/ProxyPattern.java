737830ae83e7d59ae1f17dd515c5010376ef91a0
hanmomhanda <hanmomhanda@gmail.com>
2016-01-09 11:10:54 +0900

    Proxy pattern in Javaa

--- Proxy/java/ProxyPattern.java
+++ Proxy/java/ProxyPattern.java
@@ -4,16 +4,68 @@ package Proxy.java;
  * Created by hanmomhanda on 2016-01-08.
  */
 public class ProxyPattern {
+
+    public static void main(String[] args) {
+
+        System.out.println("======= Example 1 =======");
+        Target image1 = new Proxy("Image01.jpg");
+        image1.displayImage();
+        image1.displayImage();
+        image1.displayImage();
+
+        System.out.println("======= Example 2 =======");
+        Target image2 = new Proxy("WallPaper.png");
+        image2.displayImage();
+        image2.displayImage();
+        image2.displayImage();
+    }
 }
 
 
 
-class Target {
-
+interface Target {
+    void displayImage();
 }
 
 
 
-class Proxy {
-    
+class RealTarget implements Target {
+
+    private String fileName;
+
+    public RealTarget(String fileName) {
+        this.fileName = fileName;
+        loadImageFromDisk();
+    }
+
+    private void loadImageFromDisk() {
+        System.out.println("Loading " + fileName);
+    }
+
+    @Override
+    public void displayImage() {
+        System.out.println("Displaying " + fileName);
+    }
 }
+
+
+
+class Proxy implements Target {
+
+    private String fileName;
+
+    private Target realTarget;
+
+    public Proxy(String fileName) {
+        this.fileName = fileName;
+    }
+
+    @Override
+    public void displayImage() {
+        if (realTarget == null)
+            realTarget = new RealTarget(fileName);
+        realTarget.displayImage();
+    }
+}
+
+
