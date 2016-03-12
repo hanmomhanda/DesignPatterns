8ed469f247b7651764edcb7fbf3b72313419e14b
hanmomhanda <hanmomhanda@gmail.com>
2016-03-12 17:27:09 +0900

    ecma2015 iterator 패턴 generator 추가

--- Iterator/ecma2015/IteratorPattern.js
+++ Iterator/ecma2015/IteratorPattern.js
@@ -8,23 +8,31 @@ class Book {
 class Iterable {
     constructor(object) {
         this.object = object;
-        this.object[Symbol.iterator] = () => {
-            const keys = Object.keys(this);
-            const len = keys.length;
-            let isDone = false;
-            let count = 0;
-            let next = () => {
-                if (count >= len)
-                    isDone = true;
-                let iterator = { done: isDone, value: this[keys[count++]] };
-                return iterator;
-            };
-            return { next };
-        }
+        //console.log(Object.getOwnPropertySymbols(this.object));
     }
 
-    getIterable() {
-        return this.object;
+    [Symbol.iterator]() {
+        console.log('iterator');
+        const keys = Object.keys(this.object);
+        const len = keys.length;
+        let done = false;
+        let current = 0;
+        const next = () => {
+            if (current >= len) done = true;
+            return {
+                done,
+                value: this.object[keys[current++]]
+            };
+        };
+        return { next }; // next 메서드를 가진 객체가 iterator 객체
+    }
+
+    // 두 개의 [Symbol.iterator] 중 나중에 선언된 것이
+    // 먼저 선언된 것을 덮어씀
+    *[Symbol.iterator]() {
+        console.log('generator');
+        for (let key in this.object)
+            yield this.object[key];
     }
 }
 
@@ -47,11 +55,23 @@ class Iterable {
     objectAggregate['1'] = new Book("60분 부모");
     objectAggregate['2'] = new Book("리만가설");
     objectAggregate['3'] = new Book("건투를 빈다");
-    const iterableObject = new Iterable(objectAggregate).getIterable();
 
-    let i = 0;
+    let iterableObject = new Iterable(objectAggregate);
+
     for (let item of iterableObject) {
         console.log(item);
     }
-    console.log(objectAggregate);
+
+    console.log('+++++++++++++++++++++');
+    // 원래 Array에 있는 [Symbol.iterator]도 덮어써짐
+    let test = new Iterable(arrayAggregate);
+
+    for (let item of test) {
+        console.log(item);
+    }
+
+    console.log('+++++++++++++++++++++');
+    for (let item of arrayAggregate) {
+        console.log(item);
+    }
 })();
